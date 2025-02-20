10c32f95775b3550a98895db7844501c5a75d7c7
Gabriel Silva <gabrielmg.silva@outlook.com>
2025-02-20 17:44:55 -0300

    Backend url correta do github

---
+++ src/main/java/br/com/dio/barbershoperapi/BarbershoperapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.barbershoperapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershoperapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershoperapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dio/barbershoperapi/BarbershoperapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.barbershoperapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershoperapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
